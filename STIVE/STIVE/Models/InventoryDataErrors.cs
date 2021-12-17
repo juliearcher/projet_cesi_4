0948e259d89ae152e3421374c17581c3a76c3fce
Thibaut Meunier <thibaut.meunier.13@gmail.com>
2021-12-17 16:29:08 +0100

    ItemDataErrors + InventoryDataErrors

---
+++ STIVE/STIVE/Models/InventoryDataErrors.cs
@@ -0,0 +1,116 @@
+﻿using STIVE.PrepAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STIVE.Models
+{
+	public class IventoryDataError : DataErrorModelBase, IItem
+	{
+
+		private string _documentDate;
+		public string DocumentDate
+		{
+			get => _documentDate;
+			set
+			{
+				_documentDate = value;
+				ClearErrors(nameof(DocumentDate));
+				/*if (...)
+				{
+					AddError(nameof(DocumentDate), "...");
+				}*/
+				OnPropertyChanged(DocumentDate);
+			}
+		}
+
+		private string _documentNumber;
+		public string DocumentNumber
+		{
+			get => _documentNumber;
+			set
+			{
+				_documentNumber = value;
+				ClearErrors(nameof(DocumentNumber));
+				/*if (...)
+				{
+					AddError(nameof(DocumentNumber), "...");
+				}*/
+				OnPropertyChanged(DocumentNumber);
+			}
+		}
+
+		private string _documentState;
+		public string DocumentState
+		{
+			get => _documentState;
+			set
+			{
+				_documentState = value;
+				ClearErrors(nameof(DocumentState));
+				/*if (...)
+				{
+					AddError(nameof(DocumentState), "...");
+				}*/
+				OnPropertyChanged(DocumentState);
+			}
+		}
+
+		private string _notes;
+		public string Notes
+		{
+			get => _notes;
+			set
+			{
+				_notes = value;
+				ClearErrors(nameof(Notes));
+				/*if (...)
+				{
+					AddError(nameof(Notes), "...");
+				}*/
+				OnPropertyChanged(Notes);
+			}
+		}
+
+		private string _notesClear;
+		public string NotesClear
+		{
+			get => _notesClear;
+			set
+			{
+				_notesClear = value;
+				ClearErrors(nameof(NotesClear));
+				/*if (...)
+				{
+					AddError(nameof(NotesClear), "...");
+				}*/
+				OnPropertyChanged(NotesClear);
+			}
+		}
+
+
+		public DateTime DocumentDate { get; set; }
+		public string DocumentNumber { get; set; }
+		public int DocumentState { get; set; }
+
+		public string Notes { get; set; }
+		public string NotesClear { get; set; }
+		public IEnumerable<InventoryLine> InventoryLines { get; set; }
+
+		public InventoryDataError(Inventory inventory = null)
+		{
+			if (inventory != null)
+			{
+				DocumentDate = inventory.DocumentDate;
+				DocumentNumber = inventory.DocumentNumber;
+				DocumentState = inventory.DocumentState;
+				Notes = inventory.Notes;
+				NotesClear = inventory.NotesClear;
+				
+
+			}
+		}
+	}
+}
--- STIVE/STIVE/Models/ItemDataError.cs
+++ STIVE/STIVE/Models/ItemDataError.cs
@@ -24,6 +24,199 @@ namespace STIVE.Models
 				OnPropertyChanged(Code);
 			}
 		}
+
+		private string _caption;
+		public string Caption
+        {
+			get => _caption;
+			set
+            {
+				_caption = value;
+				ClearErrors(nameof(Caption));
+				/*if (...)
+				{
+					AddError(nameof(Caption), "...");
+				}*/
+				OnPropertyChanged(Caption);
+			}
+        }
+
+		private string _description;
+		public string Description
+		{
+			get => _description;
+			set
+			{
+				_description = value;
+				ClearErrors(nameof(Description));
+				/*if (...)
+				{
+					AddError(nameof(Description), "...");
+				}*/
+				OnPropertyChanged(Description);
+			}
+		}
+
+		private string _clearDescription;
+		public string ClearDescription
+		{
+			get => _clearDescription;
+			set
+			{
+				_clearDescription = value;
+				ClearErrors(nameof(ClearDescription));
+				/*if (...)
+				{
+					AddError(nameof(ClearDescription), "...");
+				}*/
+				OnPropertyChanged(ClearDescription);
+			}
+		}
+
+		private string _notes;
+		public string Notes
+		{
+			get => _notes;
+			set
+			{
+				_notes = value;
+				ClearErrors(nameof(Notes));
+				/*if (...)
+				{
+					AddError(nameof(Notes), "...");
+				}*/
+				OnPropertyChanged(Notes);
+			}
+		}
+
+		private string _notesClear;
+		public string NotesClear
+		{
+			get => _notesClear;
+			set
+			{
+				_notesClear = value;
+				ClearErrors(nameof(NotesClear));
+				/*if (...)
+				{
+					AddError(nameof(NotesClear), "...");
+				}*/
+				OnPropertyChanged(NotesClear);
+			}
+		}
+
+		private string _realStock;
+		public string RealStock
+		{
+			get => _realStock;
+			set
+			{
+				_realStock = value;
+				ClearErrors(nameof(RealStock));
+				/*if (...)
+				{
+					AddError(nameof(RealStock), "...");
+				}*/
+				OnPropertyChanged(RealStock);
+			}
+		}
+
+		private string virtualStock;
+		public string VirtualStock
+		{
+			get => virtualStock;
+			set
+			{
+				virtualStock = value;
+				ClearErrors(nameof(VirtualStock));
+				/*if (...)
+				{
+					AddError(nameof(VirtualStock), "...");
+				}*/
+				OnPropertyChanged(VirtualStock);
+			}
+		}
+
+		private string _salePrice;
+		public string SalePrice
+		{
+			get => _salePrice;
+			set
+			{
+				_salePrice = value;
+				ClearErrors(nameof(SalePrice));
+				/*if (...)
+				{
+					AddError(nameof(SalePrice), "...");
+				}*/
+				OnPropertyChanged(SalePrice);
+			}
+		}
+
+		private string _purchasePrice;
+		public string PurchasePrice
+		{
+			get => _purchasePrice;
+			set
+			{
+				_purchasePrice = value;
+				ClearErrors(nameof(PurchasePrice));
+				/*if (...)
+				{
+					AddError(nameof(PurchasePrice), "...");
+				}*/
+				OnPropertyChanged(PurchasePrice);
+			}
+		}
+
+		private string _vat;
+		public string Vat
+		{
+			get => _vat;
+			set
+			{
+				_vat = value;
+				ClearErrors(nameof(Vat));
+				/*if (...)
+				{
+					AddError(nameof(Vat), "...");
+				}*/
+				OnPropertyChanged(Vat);
+			}
+		}
+
+		private string _itemFamilyId;
+		public string ItemFamilyId
+		{
+			get => _itemFamilyId;
+			set
+			{
+				_itemFamilyId = value;
+				ClearErrors(nameof(ItemFamilyId));
+				/*if (...)
+				{
+					AddError(nameof(ItemFamilyId), "...");
+				}*/
+				OnPropertyChanged(ItemFamilyId);
+			}
+		}
+
+		private string _supplierId;
+		public string SupplierId
+		{
+			get => _supplierId;
+			set
+			{
+				_supplierId = value;
+				ClearErrors(nameof(SupplierId));
+				/*if (...)
+				{
+					AddError(nameof(SupplierId), "...");
+				}*/
+				OnPropertyChanged(SupplierId);
+			}
+		}
+
 		public string Caption { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 		public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 		public string ClearDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -47,7 +240,19 @@ namespace STIVE.Models
 		{
 			if (item != null)
 			{
-				// TODO
+				Code = item.Code;
+				Caption = item.Caption;
+				Description = item.Description;
+				ClearDescription = item.ClearDescription;
+				Notes = item.Notes;
+				NotesClear = item.NotesClear;
+				RealStock = item.RealStock;
+				VirtualStock = item.VirtualStock;
+				SalePrice = item.SalePrice;
+				PurchasePrice = item.PurchasePrice;
+				Vat = item.Vat;
+				ItemFamilyId = item.ItemFamilyId;
+				SupplierId = item.SupplierId;
 			}
 		}
 	}
