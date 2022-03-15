79bd879a312127a90d35ff47da56f417dd434a27
Julie ARCHER <julie.archer@viacesi.fr>
2022-03-15 11:23:23 +0100

    Added password for delete button

--- STIVE/STIVE/ViewModels/MainWindowViewModel.cs
+++ STIVE/STIVE/ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace STIVE.ViewModels
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
 		public INavigator Navigator { get; set; }
+		public static AdministratorDependency Administrator { get; set; }
+		public ICommand ChangeAdminStatus => new AdministratorCommand();
 
 		public MainWindowViewModel(INavigator navigator)
 		{
 			Navigator = navigator;
+			Administrator = new AdministratorDependency();
+			Administrator.Administrator = false;
+		}
+		internal class AdministratorCommand : ICommand
+		{
+			public event EventHandler CanExecuteChanged;
+
+			public bool CanExecute(object parameter)
+			{
+				return !Administrator.Administrator;
+			}
+
+			public void Execute(object parameter)
+			{
+				Window window = new Window();
+				window.Content = new AdminPasswordWindowViewModel();
+				window.Height = 120;
+				window.Width = 300;
+				window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+				window.ShowDialog();
+			}
+		}
+
+		/*
+		 * Allows to use a changing static property in bindings
+		 */
+		public class AdministratorDependency : DependencyObject
+		{
+			public static readonly DependencyProperty AdministratorProperty =
+				DependencyProperty.Register("Administrator", typeof(bool),
+				typeof(AdministratorDependency));
+			public bool Administrator
+			{
+				get { return (bool)GetValue(AdministratorProperty); }
+				set { SetValue(AdministratorProperty, value); }
+			}
 		}
 	}
 }
---
+++ STIVE/STIVE/Views/AdminPasswordPrompt.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace STIVE.Views
+{
+	/// <summary>
+	/// Logique d'interaction pour AdminPasswordPrompt.xaml
+	/// </summary>
+	public partial class AdminPasswordPrompt : UserControl
+	{
+		public AdminPasswordPrompt()
+		{
+			InitializeComponent();
+		}
+
+		private void PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			if (this.DataContext != null)
+			{
+				((dynamic)this.DataContext).PasswordGuess = ((PasswordBox)sender).Password;
+			}
+		}
+	}
+}
