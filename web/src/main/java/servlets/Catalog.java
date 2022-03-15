15ed781ba3cd2fff23d554ddc007aa7455e47fb2
Julie ARCHER <julie.archer@viacesi.fr>
2022-03-15 13:49:36 +0100

    Searchbox for catalog

--- web/src/main/java/servlets/Catalog.java
+++ web/src/main/java/servlets/Catalog.java
@@ -5,26 +5,42 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import repositories.ItemFamilyRepository;
+import repositories.ItemRepository;
+
 import java.io.IOException;
+import java.util.ArrayList;
+
+import beans.Customer;
+import beans.Item;
+import beans.ItemFamily;
+import beans.Order;
 
 /**
  * Servlet implementation class Catalog
  */
 public class Catalog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final ItemFamilyRepository itemFamilyRepository;
+	private final ItemRepository itemRepository;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Catalog() {
         super();
-        // TODO Auto-generated constructor stub
+        itemFamilyRepository = new ItemFamilyRepository();
+        itemRepository = new ItemRepository();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<ItemFamily> itemFamilies = itemFamilyRepository.getAllItemFamilies();
+		request.setAttribute("itemFamilies", itemFamilies);
+		ArrayList<Item> items = itemRepository.getAllItems();
+		request.setAttribute("items", items);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/catalog.jsp").forward(request, response);
 	}
 
--- web/src/main/webapp/WEB-INF/catalog.jsp
+++ web/src/main/webapp/WEB-INF/catalog.jsp
@@ -1,12 +1,38 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
 <body>
 
+<input class="form-control" id="filter" type="text" placeholder="Recherche...">
+
+<div class="table-responsive">
+<table class="table table-striped table-sm">
+	<thead>
+	   <tr>
+	     <th scope="col">Code</th>
+	     <th scope="col">Libellé</th>
+	     <th scope="col">Description</th>
+	     <th scope="col">Prix de vente</th>
+	   </tr>
+	</thead>
+	<tbody id="itemListBody">
+		<c:forEach items="${ items }" var="item">
+			<tr>
+			<td><c:out value="${ item.code }" /></td>
+			<td><c:out value="${ item.caption }" /></td>
+			<td><c:out value="${ item.clearDescription }" /></td>
+			<td><c:out value="${ item.salePrice }" /></td>
+			</tr>
+		</c:forEach>
+	</tbody>
+</table>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	  $("#filter").on("keyup", function() {
+	    var value = $(this).val().toLowerCase();
+	    $("#itemListBody tr").filter(function() {
+	      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	    });
+	  });
+	});
+</script>
 </body>
-</html>
